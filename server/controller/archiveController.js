5de6d6b54cfb429035dccc1b644db47bbd1b8a73
Arunesh Saxena <asaxena24@sapient.com>
2019-03-29 17:33:50 +0530

    Archive implemented

--- server/controller/archiveController.js
+++ server/controller/archiveController.js
@@ -5,81 +5,45 @@ var rimraf = require("rimraf");
 
 module.exports = (req, res, next) => {
     const reportName = req.query.report;
-    const zippath = `public/${reportName}/${reportName}.zip`
+    const zippath = `public/${reportName}.zip`
     const type = req.query.type;
 
     if (type == 'delete') {
 
         rimraf(`./public/${reportName}`, function () {
-            console.log("File Deleted")
-            res.redirect('/list')
+            const archiveFile = `./public/${reportName}.zip`;
+            if (fs.existsSync(archiveFile)) {
+                rimraf(archiveFile, function () {
+                    console.log("File Deleted");
+                    res.redirect('/list');
+                });
+            } else {
+                console.log("File Deleted");
+                res.redirect('/list');
+            }
         });
 
     } else {
 
-        // const output = fs.createWriteStream(`./${zippath}`);
-        // const archive = archiver('zip');
+        const output = fs.createWriteStream(`./${zippath}`);
+        const archive = archiver('zip');
 
-        // output.on('close', function () {
-        //     console.log(archive.pointer() + ' total bytes');
-        //     console.log('archiver has been finalized and the output file descriptor has closed.');
-        // });
+        output.on('close', function () {
+            console.log(archive.pointer() + ' total bytes');
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+            res.download(path.join(__dirname, `../../${zippath}`));
+        });
 
-        // archive.on('error', function (err) {
-        //     throw err;
-        // });
-        // archive.pipe(output);
-        // archive.directory(`./public/${reportName}/`, true);
-        // archive.finalize();
+        archive.on('error', function (err) {
+            throw err;
+        });
+        archive.pipe(output);
+        archive.directory(`./public/${reportName}/`, false);
+        archive.finalize();
 
-        // archive.on('end', function (err) {
-        //     res.download(path.join(__dirname, `../../${zippath}`))
-        // });
-        
-        // ---------------------------------------------------------------------
-        
-        // var output = fs.createWriteStream(__dirname + "/" + zipName);
-
-        // var archive = archiver('zip');
-
-        // archive.on('error', function (err) {
-        //     throw err;
-        // });
-
-        // archive.pipe(output);
-
-        // //Add Files to Zip Archive
-        // var count = 0;
-
-        // async.each(fileList, function iterator(item, callback) {
-        //     var fileName = item.name + "/" + count + ":" + item.caption.trim() + path.extname(item.asset);
-        //     count++;
-        //     var bucket = urlPrefix.substring(0, urlPrefix.indexOf("/"));
-        //     var fileLocation = urlPrefix.substring(urlPrefix.indexOf("/") + 1) + item.asset;
-
-        //     var params = {
-        //         Bucket: bucket,
-        //         Key: fileLocation
-        //     };
-        //     s3.getObject(params, function (err, data) {
-        //         if (err) {
-        //             console.log(err);
-        //             callback(err);
-        //         } else {
-        //             console.log("zipping ", fileName, "...");
-        //             archive.append(data.Body, { name: fileName });
-        //             callback();
-        //         }
-        //     });
-        // }
-        //     , function (done) {
-        //         console.log("here");
-        //         output.on("close", function () {
-        //             console.log(archive.pointer() + " total bytes");
-        //             console.log("archiver has been finalized and the output file descriptor has closed.");
-        //         });
-        //         archive.finalize();
-        //     });
+        archive.on('end', function (err) {
+            console.log(path.join(__dirname, `../../${zippath}`));
+        });
 
     }
 }
