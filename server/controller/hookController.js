5c85242bf12bc18ae2e76e3334f7ffe8d09b488a
Gaurav Mall <gmall@sapient.com>
2019-03-30 23:39:21 +0530

    x-event deployment

--- server/controller/hookController.js
+++ server/controller/hookController.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     console.log("Incoming Request")
-    if(req.headers['x-github-event'] == 'deployment'){
+    if(req.headers['x-event'] == 'deployment'){
         res.redirect("/generateWebReport?hook=true")
     }
 }
