d687bdc1e6dae1f36f199502c56abdbbebfde466
Gaurav Mall <gmall@sapient.com>
2019-04-04 17:38:52 +0530

    different hooks for events generate|view|deployment

--- config/urlConfig.js
+++ config/urlConfig.js
@@ -1,13 +1,9 @@
 module.exports = {
-    // website: 'https://developers.google.com/products/',
+    // website: '<ENTER WEBSITE URI HERE>',
     urls: [
         {
-            name: "Lighthouse",
-            url: "https://developers.google.com/web/tools/lighthouse/"
-        },
-        {
-            name: "Input Tools",
-            url: "https://www.google.com/inputtools/"
+            name: "<ENTER PAGE NAME HERE>",
+            url: "<ENTER PAGE URL HERE>"
         }
     ],
     external: []
