290eb62aab639eaa34cb15d69a679ed67012eacd
Gaurav Mall <dev.gauravmall@gmail.com>
2023-08-19 20:13:21 +0530

    Rate limiting added

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import rateLimit from 'express-rate-limit'
 import { fileURLToPath } from 'url';
 import initRoutes from "./routes/routes.js";
 import hbs from "hbs";
@@ -9,6 +10,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Register `hbs` as our view engine using its bound `engine()` function.
